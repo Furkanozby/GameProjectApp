2d5b3eb3c910e266e62233bc92cb51ad3e5d6ed3
Furkan Özbay <furkanozb37@hotmail.com>
2021-04-18 16:50:10 +0300

    Add project files.

---
+++ GameProjectApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameProjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
